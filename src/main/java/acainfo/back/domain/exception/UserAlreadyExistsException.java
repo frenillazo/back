f97f0f7cafdd39dbc3636d0b746b70c7742f850c
PabloCalahorro <pablocalahorrojimenez@gmail.com>
2025-11-16 09:53:30 +0100

    (fix) UserAlreadyExistsException.java duplicated method erased

--- src/main/java/acainfo/back/domain/exception/UserAlreadyExistsException.java
+++ src/main/java/acainfo/back/domain/exception/UserAlreadyExistsException.java
@@ -2,10 +2,6 @@ package acainfo.back.domain.exception;
 
 public class UserAlreadyExistsException extends DomainException {
 
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-
     public UserAlreadyExistsException(String email) {
         super("User already exists with email: " + email);
     }
