4d7df56b2b7f34b4edcac0d62e36a8e809ee4f35
PabloCalahorro <pablocalahorrojimenez@gmail.com>
2025-12-03 08:45:15 +0100

    setup reimplementacion

--- src/main/java/acainfo/back/user/domain/model/RoleType.java
+++
@@ -1,7 +0,0 @@
-package acainfo.back.user.domain.model;
-
-public enum RoleType {
-    ADMIN,
-    TEACHER,
-    STUDENT
-}
--- src/main/java/acainfo/back/user/domain/model/UserStatus.java
+++
@@ -1,8 +0,0 @@
-package acainfo.back.user.domain.model;
-
-public enum UserStatus {
-    ACTIVE,
-    INACTIVE,
-    SUSPENDED,
-    PENDING_VERIFICATION
-}
