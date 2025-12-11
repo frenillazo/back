0f2a211d7b150705f4a7e616a40247a95eb06b32
PabloCalahorro <pablocalahorrojimenez@gmail.com>
2025-12-11 12:21:06 +0100

    (fix) types of session adapted

--- src/main/java/com/acainfo/session/domain/model/SessionType.java
+++ src/main/java/com/acainfo/session/domain/model/SessionType.java
@@ -3,5 +3,5 @@ package com.acainfo.session.domain.model;
 public enum SessionType {
     REGULAR, //Sesión instanciada desde un Schedule
     EXTRA, //Sesión que se crea ajena a los schedules para repasar, hacer un simulacro de examen o corregir alguna tarea de forma extraordinaria
-    SCHEDULING //Tipo de reunión online de ocurrencia única cuyo objetivo es acordar horarios con los alumnos al configurar un grupo nuevo
+    SCHEDULING //Tipo de reunión online de ocurrencia única cuyo objetivo es acordar horarios con los alumnos al configurar un grupo o grupos nuevos
 }
